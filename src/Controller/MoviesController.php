423b68723f9d86ed4140883365b940edf6d48d3c
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2024-06-12 07:48:43 +0100

    controller

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,16 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
     #[Route('/movies', name: 'app_movies')]
-    public function index(): JsonResponse
+    public function index(): Response
     {
+        return $this->render('index.html.twig', ['title' => '']);
+    }
+
+    #[Route('/movies/{name}', name: 'movies', defaults:['name' => null], methods:['GET', 'HEAD'])]
+    public function getMovie($name): JsonResponse
+    {
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
+            'message' => $name,
             'path' => 'src/Controller/MoviesController.php',
         ]);
     }
